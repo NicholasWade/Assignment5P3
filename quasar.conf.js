7ae93c91f06203fc977619ecb2356fc81696eff6
NicholasWade <nwade@unomaha.edu>
2020-11-23 03:06:16 -0600

    first commit

--- quasar.conf.js
+++ quasar.conf.js
@@ -176,6 +176,8 @@ module.exports = function (/* ctx */) {
       packager: {
         // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
 
+        platform: 'darwin'
+
         // OS X / Mac App Store
         // appBundleId: '',
         // appCategoryType: '',
